1836e5e1673e3ec411ad4e17a7a0d1f47023a4f2
Louis Abery <louis@abery.com>
2026-05-12 00:17:43 +0100

    redirect 404 to home

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,8 @@
 <script>
     import { page } from '$app/state';
+    import { resolve } from '$app/paths';
 </script>
 
 <h1>Oops! {page.status}</h1>
 <p>{page.error?.message || 'Page not found'}</p>
+<a href={resolve('/')}>Go Home</a>
