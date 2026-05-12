49c15d93736b34a7446c9ed322e2413886ddf39f
aybery <louis@abery.com>
2026-05-12 16:43:19 +0100

    reformatting nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /><title>Louis Abery Portfolio</title></svelte:head>
 
-<nav class="p-6 border-b border-black gap-6 items-center">
-    <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-    </ul>
+<nav class="p-6 flex gap-6 items-center border-b border-black">
+    <a href={resolve('/#home')}>Home</a>
+    <a href={resolve('/#projects')}>Projects</a>
+    <a href={resolve('/#contact')}>Contact</a>
 </nav>
 
 {@render children()}
 
 <footer class="flex items-center justify-center p-4">
-    <p>&copy; 2026 Louis Abery</p>
+    <p>&copy; 2026 Louis Abery.</p>
 </footer>
